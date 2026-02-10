13968b216dde5590015e05606ebd47e61f042df0
ansari-fahad <ansari61814@gmail.com>
2026-02-11 01:06:21 +0530

    2url

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -22,8 +22,7 @@ const Login = () => {
 
     try {
       const response = await fetch(
-        `${import.meta.env.VITE_API_URL}/api/users/login`, {
-        
+        "https://storebackendapp.vercel.app/api/users/login", {
         method: 'POST',
         headers: {
           'Content-Type': 'application/json',
